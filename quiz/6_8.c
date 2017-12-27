95831197c88f8b8bc2d5100e6d05e4c5eeabd653
Mama Stitch <aharmonisesair@gmail.com>
2017-12-27 19:31:41 +0900

    20171227 scanf_int()追加

--- quiz/6_8.c
+++ quiz/6_8.c
@@ -8,18 +8,31 @@ int ave_of(int a,int b){
     return (int)((a + b) / 2);
 
 }
-
+/*
+int型の整数値を読み込んで返却
+*/
+int scan_int(void){
+    int temp;
+    printf("整数値:");
+    scanf("%d",&temp);
+    return temp;
+}
 int main(void){
 
-    int a,b;
+    int a,b,ave;
     setvbuf(stdout,NULL,_IONBF,0);
 
-    printf("a = ");
-    scanf("%d",&a);
-    printf("b = ");
-    scanf("%d",&b);
+    a = scan_int();
+    b = scan_int();
     
-    printf("average = %d\n",ave_of(a,b));
+    if((ave = ave_of(a,b)) == 0){
+        printf("aとbの平均値は0 ,ave =%d\n",ave);
+    }else if(ave >0){
+        printf("aとbの平均値は正,ave =%d\n",ave);
+    }else{
+        printf("aとbの平均値は負,ave =%d\n",ave);
+    }
+
     return 0;
 
 }
