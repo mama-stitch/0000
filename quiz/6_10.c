11045c9844d0a6ece5b90d5fdb96199b1ca3599c
Mama Stitch <aharmonisesair@gmail.com>
2017-12-28 19:28:33 +0900

    変更:int型array配列を構造体STUDENT配列に変更 関数も構造体に対応できるように変更

--- quiz/6_10.c
+++ quiz/6_10.c
@@ -2,35 +2,58 @@
 /*
 int型、要素数nの配列の最小値の要素を返却する
 */
-#define NUMBER 5
-int scanf_int(int *num,int n){
+#define NUMBER 5 
+typedef struct{
+
+    int math;
+    int lang;
+
+}STUDENT;
+
+int scanf_int(STUDENT *student,int n){
     int i;
     for(i = 0; i < n; i++){
-        printf("array[%d] = ",i);
-        scanf("%d",&num[i]);
+        printf("student[%d]\n",i);
+        printf("    math =");
+        scanf("%d",&(student[i].math));
+        printf("    lang =");
+        scanf("%d",&(student[i].lang));
     }
 }
-void print_array(int *array,int n){
+void print_array(STUDENT *student,int n){
     int i;
-    for(i = 0; i < n; i++)
-        printf("array[%d] = %d\n",i,array[i]);
-}
-int min_of(const int *array, int n){
-    int i;
-    int min = array[0];
     for(i = 0; i < n; i++){
-        if(min > array[i])
-            min = array[i];
+        printf("student[%d]\n",i);
+        printf("    math = %d\n",student[i].math);
+        printf("    lang = %d\n",student[i].lang);
+    }
+}
+int min_of_math(const STUDENT *student, int n){
+    int i;
+    int min = student[0].math;
+    for(i = 0; i < n; i++){
+        if(min > student[i].math)
+            min = student[i].math;
+    }
+    return min;
+
+}
+int min_of_lang(const STUDENT *student, int n){
+    int i;
+    int min = student[0].lang;
+    for(i = 0; i < n; i++){
+        if(min > student[i].lang)
+            min = student[i].lang;
     }
     return min;
 
 }
 int main(void){
     setvbuf(stdout,NULL,_IONBF,0);
-    int array[NUMBER];
-
-    scanf_int(array,NUMBER);
-    print_array(array,NUMBER);
-    printf("min = %d \n",min_of(array,NUMBER));
+    STUDENT students[NUMBER];
+    scanf_int(students,NUMBER);
+    print_array(students,NUMBER);
+    printf("min_math = %d \n",min_of_math(students,NUMBER));
+    printf("min_lang = %d \n",min_of_lang(students,NUMBER));
     return 0;
 }
