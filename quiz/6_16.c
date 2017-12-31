e5a85b854b8b1cc61d862bde74123a0040880f38
Mama Stitch <aharmonisesair@gmail.com>
2017-12-31 21:03:35 +0900

    関数内からテスト回数を除去し、引数の配列の次元を3次元から2次元とした。テスト回数はmain()へ移した

--- quiz/6_16.c
+++ quiz/6_16.c
@@ -1,65 +1,52 @@
 #include<stdio.h>
 /*
-学生    　　4人
-科目　  　  3科目
-テスト回数  2回
+a[][],b[][]を1回目2回目とみる
+1回目2回目の合計値をsumに格納する
+numは人数
 
-点数は3次元配列
-合計値出力関数
-
-students[テスト回数][人数][科目数]
-{
-    {{60,70,90} ,{55,65,85} ,{72,82,92},{56,67,89}}
-    ,{{60,70,90} ,{55,65,85} ,{72,82,92},{98,99,100}}
-}
 */
-int exam_count = 2;
-int num = 4;
-int sbj_num = 3;
-int sum[4][3] = {0};
-int students[2][4][3] = {
-        {{60,70,90} ,{55,65,85} ,{72,82,92},{56,67,89}},
-        {{60,70,90} ,{55,65,85} ,{72,82,92},{98,99,100}},
-    };
-void mat_add(const int students[][4][3],int sum[][3],const int n){
-    int i,j,k;
-    //合計値
-    for(i = 0; i < n; i++){
-        for(j = 0; j < sbj_num; j++){
-            for(k = 0; k < exam_count; k++)
-                sum[i][j] += students[k][i][j];
-        }
-    }
-}
-void print_array(){
-    printf("output \n");
-    int i,j,k;
-    for(i = 0; i < exam_count; i++){
-        for(j = 0; j < num; j++){
-            for(k = 0; k < sbj_num; k++){
-                printf("students[%d][%d][%d] = %d\n"
-                                ,i,j,k,students[i][j][k]);
-            }
-        }
-    
-    }
-}
-void print_mat(int sum[][3],int sbj_num,int num){
-    //合計値出力
+void mat_add(int a[][3],int b[][3],int sum[][3],int num){
     int i,j;
     for(i = 0; i < num; i++){
-        for(j = 0; j < sbj_num; j++){
-            printf("sum[%d][%d] = %d\n",i,j,sum[i][j]);
+        for(j = 0; j < 3; j++){
+            sum[i][j] = a[i][j] + b[i][j];
+            printf("sum[%d][%d] = a[%d][%d] + b[%d][%d]\n\n"
+                ,i,j,i,j,i,j);
         }
     }
 }
-int main(void){
-    setvbuf(stdout,NULL,_IONBF,0);
-    mat_add(students, sum,num);
-    print_array();
-    print_mat(sum,sbj_num,num);
-   return 0;
+void mat_print(int points[][3],int num){
+    int i,j;
+    for(i = 0; i < num; i++){
+        for(j = 0; j < 3; j++){
+            printf(" %3d ",points[i][j]);
+        }
+        putchar('\n');
+    }
+
 }
+int main(void){
+    int i;
+    int points[][4][3] = {
+        {{91,63,78},{67,72,46},{89,34,53},{32,54,34},},//points[0]
+        {{97,67,82},{73,43,46},{97,56,21},{85,46,35},},//points[1]
+    };
+    int sum[4][3];
+    /*
+    mat_add(1回目点数,2回目点数,合計,人数);
+    */
+    mat_add(points[0],points[1],sum,4);
 
+    //各回の点数を表示
+    //mat_print(i回目の点数,人数)
+    for(i = 0; i < 2; i++){
+        printf("%d回目の点数\n",i);
+        mat_print(points[i],4);
+    }
+    //合計点表示
+    //mat_print(合計の点数,人数)
+    printf("合計点数\n");
+    mat_print(sum,4);
+    return 0;
 
-
+}
