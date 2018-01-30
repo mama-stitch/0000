38531c49d9879cc7bcbb5ff1342aefe3433ad91a
Mama Stitch <aharmonisesair@gmail.com>
2018-01-31 00:02:45 +0900

    [new]数当てゲーム（答えランダム）

---
+++ minigame/random3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void){
+    setvbuf(stdout,NULL,_IONBF,0);
+    srand(time(NULL));
+
+    int ans = rand();
+    int n;
+
+    do{
+        printf("答えは>> ");scanf("%d",&n);
+        if(ans > n){
+            printf("答えはもっと大きい\n");
+        }else if(ans < n){
+            printf("答えはもっと小さい\n");
+        }else{
+            printf(":l@;/:l;:/l@@;正解@@;:.@l;\n");
+        }
+    }while(ans != n);
+
+    printf("ans = %d\n",ans);
+    return 0;
+
+}
