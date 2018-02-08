9593e11b17e9c17e3240d6f08298e7b500f3f5ee
Mama Stitch <aharmonisesair@gmail.com>
2018-02-09 02:05:33 +0900

    [追加]一様確立の状態から出現確立に偏りを追加

--- minigame/kazuate/omikuji.c
+++ minigame/kazuate/omikuji.c
@@ -4,26 +4,70 @@
 /*
     おみくじ
 */
+typedef struct{
+    int freq;        //出現確立 まだ使ってない 
+    char *str;       //くじ内容
+    int count;       //出現回数
+}KUJI;
+
+
 int main(void){
     setvbuf(stdout,NULL,_IONBF,0);
-    char kuji[][127] = {
-        "大吉"      //0
-        ,"中吉"     //1
-        ,"末吉"     //2
-        ,"吉"       //3
-        ,"凶"       //4
-        ,"大凶"     //5
+/*    char kuji[][127] = {
+        名前        index   出現確立
+        "大吉"      //0     20
+        ,"中吉"     //1     30
+        ,"吉"       //2     20
+        ,"末吉"     //3     15
+        ,"凶"       //4     10
+        ,"大凶"     //5     5
     };
+*/
 
     srand(time(NULL));
-    int n = rand() % 6;
+    KUJI kuji[] ={
+        {79,"大吉",0}
+        ,{49,"中吉",0}
+        ,{79,"吉",0}
+        ,{24,"末吉",0}
+        ,{14,"凶",0}
+        ,{5,"大凶",0}
+    };
+    int n;
+    int retry;
+    //くじ出現確立を変更
+    do{
+        n = rand() % 100;
+        printf("%d\n",n);
+        if(n > 79){
+            printf("大吉\n");      //0     20
+            kuji[0].count++;
+        }else if(n > 49){
+            printf("中吉\n");       //1     30
+            kuji[1].count++;
+        }else if(n > 24){
+            printf("吉\n");       //2     20 
+            kuji[2].count++;
+        }else if(n > 14){
+            printf("末吉\n");     //3     15
+            kuji[3].count++;
+        }else if(n > 9){
+            printf("凶\n");       //4     10
+            kuji[4].count++;
+        }else if(n > 4){
+            printf("大凶\n");     //5     5
+            kuji[5].count++;
+        }
+        printf("retry?\nYES:1   NO:0\n>> ");
+        scanf("%d",&retry);
+    }while(retry == 1);
+
+    //くじ実績表示
     int i;
-    printf("---%s---\n",kuji[n]);
-/*
-    for(i = 0; i < 6; i++){
-        printf("%s\n",kuji[i]);
+    for(i = 0;i < 6; i++){
+       printf("%s : %d\n" ,kuji[i].str,kuji[i].count);
+
     }
-*/
     return 0;
 }
 
