3965945577214bb19f796d9d1c298fa4db27f5f1
Mama Stitch <aharmonisesair@gmail.com>
2018-02-15 01:07:00 +0900

    [追加]おみくじの結果の出現確率設定機能追加

--- minigame/kazuate/omikuji.c
+++ minigame/kazuate/omikuji.c
@@ -5,7 +5,7 @@
     おみくじ
 */
 typedef struct{
-    int freq;        //出現確立 まだ使ってない 
+    int prob;        //出現確立 まだ使ってない 
     char *str;       //くじ内容
     int count;       //出現回数
 }KUJI;
@@ -26,46 +26,98 @@ int main(void){
 
     srand(time(NULL));
     KUJI kuji[] ={
-        {79,"大吉",0}
-        ,{49,"中吉",0}
-        ,{79,"吉",0}
-        ,{24,"末吉",0}
-        ,{14,"凶",0}
-        ,{5,"大凶",0}
+         {0,"大吉",0}
+        ,{0,"中吉",0}
+        ,{0,"吉  ",0}
+        ,{0,"末吉",0}
+        ,{0,"凶  ",0}
+        ,{0,"大凶",0}
     };
     int n;
     int retry;
-    //くじ出現確立を変更
+    int i,j;
+    int all_prob = 100;
+    int prob;
+   
+    printf("確率の設定\n");
+    for(i = 0; i < 6; i++){
+        do{
+            printf("%sの確率",kuji[i].str);
+            printf("%d : ",i);scanf("%d",&kuji[i].prob);
+        }while(kuji[i].prob < 0 || kuji[i].prob > 100);
+
+        if((all_prob - kuji[i].prob) <= 0){
+            kuji[i].prob = all_prob;
+            for(j = i + 1; j < 6;j++){ 
+                kuji[j].prob = 0;
+                printf("%s,%d\n",kuji[j].str,kuji[j].prob);
+            }
+            break;
+        }
+        all_prob -= kuji[i].prob;
+        if(i == 5) 
+            kuji[i].prob = all_prob; 
+    
+        printf("all_prob = %d\n",all_prob);
+    
+    }
+    /*
+    printf("確率設定test \n");
+    for(i = 0; i < 6; i++){
+        printf("%s : %d\n",kuji[i].str,kuji[i].prob);
+    }
+    */
+
+
+
+    /*
+    おみくじの確立を昇順にソートする
+    乱数nの値と比較する
+    昇順にソートされた確立と乱数nを比較する
+    乱数が確立を下回ればif文をtureで出力
+    n = rand() % 100;
+    sort(kuji);
+
+    kuji[i].prob    10  20      30          20      10  10
+                    |   |       |           |       |   |
+srt kuji[i].prob    10  10  10  20      30
+                    |   |   |   |       |               |
+
+               乱数n を↑のおみくじ確率の枠に投げ込むイメージ
+*/
     do{
+        
         n = rand() % 100;
         printf("%d\n",n);
-        if(n > 79){
-            printf("大吉\n");      //0     20
+        /*
+        条件式変更予定
+        if(n < kuji[0].prob){
+            printf("大吉\n");     //0     20
             kuji[0].count++;
-        }else if(n > 49){
-            printf("中吉\n");       //1     30
+        }else if(n > kuji[1].prob){
+            printf("中吉\n");     //1     30
             kuji[1].count++;
-        }else if(n > 24){
+        }else if(n > kuji[2].prob){
             printf("吉\n");       //2     20 
             kuji[2].count++;
-        }else if(n > 14){
+        }else if(n > kuji[3].prob){
             printf("末吉\n");     //3     15
             kuji[3].count++;
-        }else if(n > 9){
+        }else if(n > kuji[4].prob){
             printf("凶\n");       //4     10
             kuji[4].count++;
-        }else if(n > 4){
+        }else if(n > kuji[5].prob){
             printf("大凶\n");     //5     5
             kuji[5].count++;
         }
+        */
         printf("retry?\nYES:1   NO:0\n>> ");
         scanf("%d",&retry);
     }while(retry == 1);
 
     //くじ実績表示
-    int i;
     for(i = 0;i < 6; i++){
-       printf("%s : %d\n" ,kuji[i].str,kuji[i].count);
+       printf("%s : %d : %d%\n" ,kuji[i].str,kuji[i].count,kuji[i].prob);
 
     }
     return 0;
