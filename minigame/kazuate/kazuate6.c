10ef9b4e2b41758c6cffb5c4b2ece734164ccc1c
Mama Stitch <aharmonisesair@gmail.com>
2018-02-17 23:27:27 +0900

    [追加]入力履歴機能追加

--- minigame/kazuate/kazuate6.c
+++ minigame/kazuate/kazuate6.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+
 /*
-
-
-残りゲーム回数
 実行ゲーム回数
 追加予定
 入力履歴保存
@@ -15,31 +13,43 @@ int main(void){
     setvbuf(stdout,NULL,_IONBF,0);
     srand(time(NULL));
 
-    int num,ans;
-    int i;
-    int game_count = 10;
+    int num;
+    int remains = 10;
+    int game_count = 0;
+    int history[10]; 
 
-    ans = rand() % 2000 - 1000;
-
-    do{    
-        game_count--;
-
+    printf("-999　～ 999　のあいだの数を当ててください \n");
+    int ans = rand() % 2000 - 1000;
+    do{
         do{
             printf("num >> ");scanf("%d",&num);
         }while(1000 < num || -1000 > num);
 
-        if(ans > num)
-            printf("ansはもっと大きい : gamecount %2d",game_count);
-        else if(ans < num)
-            printf("ansはもっと小さい : gamecount %2d",game_count);
+        if(remains >= 0){
+            history[game_count] = num;
+            game_count++;
+        }
+    
 
-    }while(game_count >= 0 && ans != num);
+        remains--;
+
+        if(ans > num)
+            printf("ansはもっと大きい : %2d回目 残り%2d回\n",game_count,remains);
+        else if(ans < num)
+            printf("ansはもっと小さい : %2d回目 残り%2d回\n",game_count,remains);
+        putchar('\n');
+
+    }while(0 < remains && ans != num);
 
     if(ans == num){
-        printf("正解 :  %d : gamecount :%d\n",ans,game_count);
+        printf("正解     :  %d : %2d回目 残り%2d回\n",ans,game_count,remains);
     }else{
-        printf("残念 :  %d : gamecount :%d\n",ans,game_count);
+        printf("残念 答え:  %d : %2d回目 残り%2d回\n",ans,game_count,remains);
     }
 
+    int i;
+    for(i = 0; i < game_count; i++){
+        printf("history[%d] = %d\n",i,history[i]);
+    }
     return 0;
 }
