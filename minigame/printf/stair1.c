0c85abf47bdd0cbb684bedfc757848da208b71df
Mama Stitch <aharmonisesair@gmail.com>
2018-02-22 22:55:26 +0900

    ...

--- minigame/printf/stair1.c
+++ minigame/printf/stair1.c
@@ -6,9 +6,11 @@
 int main(void){
 
     setvbuf(stdout,NULL,_IONBF,0);
+
     int i,j;
     int num;
     printf("階段の段数は？ >> ");scanf("%d",&num);
+
     for(i = 1; i <= num; i++){
         for(j = 1;j <= i; j++){
             if(j < i)
